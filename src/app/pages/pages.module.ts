96b1c07f856ca234a084b6a5c0e12c7bc8614029
Valeria Del Río <vdelrio@grupoassa>
2018-05-28 16:59:05 -0300

    seccion 8 - clase 73 terminada

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -18,6 +18,8 @@ import { FormsModule } from '@angular/forms';
 // Graficas
 import { ChartsModule } from 'ng2-charts';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 @NgModule({
   imports: [
@@ -33,7 +35,9 @@ import { AccountSettingsComponent } from './account-settings/account-settings.co
     PagesComponent,
     IncrementadorComponent,
     GraficoDonaComponent,
-    AccountSettingsComponent
+    AccountSettingsComponent,
+    PromesasComponent,
+    RxjsComponent
   ],
   exports: [
     DashboardComponent,
--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -5,6 +5,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 const ROUTES: Routes = [
   {
@@ -14,10 +16,12 @@ const ROUTES: Routes = [
       {path: 'dashboard', component: DashboardComponent},
       {path: 'progress', component: ProgressComponent},
       {path: 'graficas1', component: Graficas1Component},
+      {path: 'promesas', component: PromesasComponent},
+      {path: 'rxjs', component: RxjsComponent},
       {path: 'account-settings', component: AccountSettingsComponent},
       {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
     ]
   },
 ];
 
-export const PAGES_ROUTES = RouterModule.forChild(ROUTES)
+export const PAGES_ROUTES = RouterModule.forChild(ROUTES);
